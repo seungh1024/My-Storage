086607917898fd2a5034645d3bce3e242331b450
seungh1024 <seungh1024@naver.com>
2024-11-30 02:10:17 +0900

    refactor: 미사용 파일 제거

--- src/main/java/com/woowacamp/storage/domain/folder/repository/FolderMetadataJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.woowacamp.storage.domain.folder.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.woowacamp.storage.domain.folder.entity.FolderMetadata;
-
-public interface FolderMetadataJpaRepository extends JpaRepository<FolderMetadata, Long> {
-}
