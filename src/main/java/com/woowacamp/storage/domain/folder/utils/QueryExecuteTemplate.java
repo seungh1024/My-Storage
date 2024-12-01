48d841e0a9df0449686fba4773cafb9b9e018b93
seungh1024 <seungh1024@naver.com>
2024-12-02 01:15:44 +0900

    refactor: 쿼리 템플릿 수정 - 일반 타입도 제네릭 타입으로 전부 변경 - 변경에 따른 불필요한 타입과 타입 변경은 제거

--- src/main/java/com/woowacamp/storage/domain/folder/utils/QueryExecuteTemplate.java
+++ src/main/java/com/woowacamp/storage/domain/folder/utils/QueryExecuteTemplate.java
@@ -15,31 +15,29 @@ import lombok.extern.slf4j.Slf4j;
 public class QueryExecuteTemplate {
 
 	/**
-	 * 필요한 파일을 select 수행한 결과로 addFunction으로 pk만 선별한 후, 해당 결과로 비즈니스 로직을 수행
+	 * 필요한 파일을 select 수행한 결과로 비즈니스 로직을 수행
 	 * stack 기반으로 DFS 탐색함
 	 *
 	 * @param selectFunction
-	 * @param addFunction
 	 * @param resultConsumer
 	 * @param <T>
 	 */
-	public static <T> void selectFilesAndExecute(Function<Long, List<T>> selectFunction,
-		Function<List<T>, List<Long>> addFunction, Consumer<List<T>> resultConsumer) {
-		Stack<Long> stack = new Stack<>();
+	public static <T> void selectFilesAndExecute(Function<T, List<T>> selectFunction,
+		Consumer<List<T>> resultConsumer) {
+
+		Stack<T> stack = new Stack<>();
 		do {
 			log.info("[Stack] {}", stack);
 			List<T> selectList = selectFunction.apply(stack.size() > 0 ? stack.pop() : null);
 			log.info("[After Stack] {}", stack);
-			List<Long> folderPkList = addFunction.apply(selectList);
-			log.info("[FolderPkList] {}", folderPkList);
-			stack.addAll(folderPkList);
+			stack.addAll(selectList);
 			resultConsumer.accept(selectList);
 
 		} while (!stack.isEmpty());
 	}
 
 	/**
-	 * select로 실행한 결과를 모아서 한 번만 비즈니스 로직을 수행
+	 * select로 실행한 결과를 모아서 U타입으로 가공 후, 한 번만 비즈니스 로직을 수행
 	 * 용량 업데이트, 권한 수정 등 조회한 여러 값들을 하나의 값으로 변경하기 위해 사용
 	 *
 	 * @param selectFunction
@@ -47,20 +45,22 @@ public class QueryExecuteTemplate {
 	 * @param resultConsumer
 	 * @param <T>
 	 */
-	public static <T> void selectFilesAndBatchExecute(Function<Long, List<T>> selectFunction,
-		Function<List<T>, List<Long>> addFunction, Consumer<List<Long>> resultConsumer){
-		Stack<Long> stack = new Stack<>();
-		List<Long> idList = new ArrayList<>();
+	public static <T, U> void selectFilesAndBatchExecute(Function<T, List<T>> selectFunction,
+		Function<List<T>, List<U>> addFunction, Consumer<List<U>> resultConsumer) {
+
+		Stack<T> stack = new Stack<>();
+		List<U> idList = new ArrayList<>();
+
 		do {
 			log.info("[Stack] {}", stack);
 			List<T> selectList = selectFunction.apply(stack.size() > 0 ? stack.pop() : null);
 			log.info("[After Stack] {}", stack);
-			List<Long> folderPkList = addFunction.apply(selectList);
+			List<U> folderPkList = addFunction.apply(selectList);
 			log.info("[FolderPkList] {}", folderPkList);
-			stack.addAll(folderPkList);
+			stack.addAll(selectList);
 			idList.addAll(folderPkList);
 
-		} while (!stack.isEmpty());
+		} while (stack.peek() != null && !stack.isEmpty());
 		resultConsumer.accept(idList);
 	}
 }
