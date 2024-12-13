c6abad1cc68ea08115933ac615d8d4280a2a0680
seungh1024 <seungh1024@naver.com>
2024-12-14 01:36:27 +0900

    feat: 분산락 관리 메소드 추가 - 비즈니스 로직 결과를 반환하는 경우가 필요하기 때문에 원하는 타입을 반환하는 락 관리 메소드 추가

--- src/main/java/com/woowacamp/storage/domain/folder/service/RedisLockService.java
+++ src/main/java/com/woowacamp/storage/domain/folder/service/RedisLockService.java
@@ -1,6 +1,7 @@
 package com.woowacamp.storage.domain.folder.service;
 
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
@@ -18,7 +19,7 @@ public class RedisLockService {
 	private final int takingLockTime = 1;
 	private final int keepingLockTime = 10;
 
-	public <T extends RuntimeException> void handleUserRequest(String lockName, Runnable task, T exception) {
+	public void handleUserRequest(String lockName, Runnable task, RuntimeException exception) {
 		RLock lock = redissonClient.getLock(lockName);
 		boolean isLocked = false;
 
@@ -26,7 +27,6 @@ public class RedisLockService {
 			isLocked = lock.tryLock(takingLockTime, keepingLockTime, TimeUnit.SECONDS);
 			// 락 획득 실패 시 동시 요청이므로 예외 던짐
 			if (!isLocked) {
-				System.out.println("???");
 				throw exception;
 			}
 			task.run();
@@ -35,9 +35,45 @@ public class RedisLockService {
 			throw new RuntimeException(e);
 		} finally {
 			if (isLocked) {
-				lock.unlock();
+				try {
+					lock.unlock();
+				} catch (IllegalMonitorStateException e) {
+					// 락 소유 시간동안 비즈니스 로직 처리를 하지 못한 경우 예외 처리
+					// 여기서 throw를 하면 비즈니스 로직이 정상적으로 처리됐어도 예외 응답을 받게 됨.
+					// 메일 같은 것으로 락 소유시간이 짧은 것 같다는 알림을 전송하는게 좋다고 생각
+					log.error("Failed to do service: " + e.getMessage(), e);
+				}
 			}
 		}
+	}
+
+	public <T> T handleUserRequest(String lockName, Supplier<T> task, RuntimeException exception) {
+		RLock lock = redissonClient.getLock(lockName);
+		boolean isLocked = false;
+		T result = null;
+
+		try {
+			isLocked = lock.tryLock(takingLockTime, keepingLockTime, TimeUnit.SECONDS);
+			if (!isLocked) {
+				throw exception;
+			}
+			lock.lock();
+
+			result = task.get();
+		} catch (InterruptedException e) {
+			log.error("[RedisLockService] error = {}", e);
+			throw new RuntimeException(e);
+		} finally {
+			if (isLocked) {
+				try {
+					lock.unlock();
+				} catch (IllegalMonitorStateException e) {
+					log.error("Failed to do service: " + e.getMessage(), e);
+				}
+			}
+		}
+
+		return result;
 	}
 
 }
