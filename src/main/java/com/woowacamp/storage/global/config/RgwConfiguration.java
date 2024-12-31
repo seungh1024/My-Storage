a677c007a53698814af87ddb7b3fdf1e11474837
seungh1024 <seungh1024@naver.com>
2024-12-31 19:53:09 +0900

    refactor: S3Configuration 이름 변경 - RgwConfiguration으로 변경

---
+++ src/main/java/com/woowacamp/storage/global/config/RgwConfiguration.java
@@ -0,0 +1,50 @@
+package com.woowacamp.storage.global.config;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+@Configuration
+@Profile({"local"})
+public class RgwConfiguration {
+
+	@Value("${cloud.aws.credentials.accessKey}")
+	private String accessKey;
+
+	@Value("${cloud.aws.credentials.secretKey}")
+	private String secretKey;
+
+	@Value("${cloud.aws.credentials.endpoint}")
+	private String endpoint;
+
+	@Bean
+	public S3Client s3Client() {
+		return S3Client.builder()
+			.endpointOverride(URI.create(endpoint))
+			.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+			.region(Region.AP_NORTHEAST_2)
+			.build();
+	}
+
+
+	@Bean
+	public S3Presigner s3Presigner() {
+		return S3Presigner.builder()
+			.endpointOverride(URI.create(endpoint))
+			.credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+			.region(Region.AP_NORTHEAST_2)
+			.serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
+			.build();
+	}
+
+}
