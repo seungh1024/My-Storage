35ccc230913c386721bfe4577ada915ffb36e723
seungh1024 <seungh1024@naver.com>
2024-12-31 19:54:15 +0900

    feat: presigned url 처리 클래스 추가 - presigned url 방식으로 업로드를 할 수 있는 url 제공 - presigned url 방식으로 다운로드를 할 수 있는 url 제공

---
+++ src/main/java/com/woowacamp/storage/domain/file/service/PresignedUrlService.java
@@ -0,0 +1,69 @@
+package com.woowacamp.storage.domain.file.service;
+
+import java.net.URL;
+import java.time.Duration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+@Service
+@RequiredArgsConstructor
+public class PresignedUrlService {
+	private final S3Presigner s3Presigner;
+
+	@Value("${cloud.aws.credentials.bucketName}")
+	private String bucketName;
+	@Value("${cloud.aws.credentials.duaration}")
+	private int duration;
+
+	private PutObjectRequest getPutObjectRequest(String objectKey) {
+		return PutObjectRequest.builder()
+			.bucket(bucketName)
+			.key(objectKey)
+			.build();
+	}
+
+	/**
+	 * presigned url을 생성하는 메서드
+	 * @param objectKey
+	 * @return
+	 */
+	public URL getPresignedUrl(String objectKey) {
+		// 객체 업로드를 위한 객체 정보 설정
+		PutObjectRequest objectRequest = getPutObjectRequest(objectKey);
+
+		// presigned url 설정
+		PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+			.signatureDuration(Duration.ofMinutes(duration))
+			.putObjectRequest(objectRequest)
+			.build();
+
+		// presigned url 생성 및 url 반환
+		return s3Presigner.presignPutObject(presignRequest).url();
+	}
+
+	private GetObjectRequest getGetObjectRequest(String objectKey) {
+		return GetObjectRequest.builder()
+			.bucket(bucketName)
+			.key(objectKey)
+			.build();
+	}
+
+	public URL getDownloadUrl(String objectKey) {
+		GetObjectRequest objectRequest = getGetObjectRequest(objectKey);
+
+		GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+			.signatureDuration(Duration.ofMinutes(duration))
+			.getObjectRequest(objectRequest)
+			.build();
+
+		return s3Presigner.presignGetObject(presignRequest).url();
+	}
+}
