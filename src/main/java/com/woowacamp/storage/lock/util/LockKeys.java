5b01198d92e85a80f95a63caa09d3098ed169262
seungh1024 <seungh1024@naver.com>
2026-01-17 23:00:26 +0900

    feat : Redis 분산락 키 포맷 빈 추가 - 중복 이름, 수정 작업 단위로 락 제공

---
+++ src/main/java/com/woowacamp/storage/lock/util/LockKeys.java
@@ -0,0 +1,17 @@
+package com.woowacamp.storage.lock.util;
+
+import org.springframework.stereotype.Component;
+
+@Component("lockKeys") // SpEL에서 @lockKeys로 접근
+public class LockKeys {
+	public static final String FOLDER_JOB = "folder job:";
+	public static final String FOLDER_NAME = "folder name:";
+
+	public String folderJob(Long id) {
+		return FOLDER_JOB.concat(id.toString());
+	}
+
+	public String folderName(Long parentId, String folderName) {
+		return FOLDER_NAME.concat(parentId.toString()).concat("/").concat(folderName);
+	}
+}
