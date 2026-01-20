1799e7954263dded0830e26510c9f1a9a65bb1a1
seungh1024 <seungh1024@naver.com>
2026-01-21 03:55:13 +0900

    feat: Service 호출 분기용 클래스 추가 - Controller -> Facade -> Service로 호출 - Facade에서 락 획득 메서드 호출 이후 나머지 비즈니스 로직 호출

---
+++ src/main/java/com/woowacamp/storage/domain/folder/facade/FolderFacade.java
@@ -0,0 +1,25 @@
+package com.woowacamp.storage.domain.folder.facade;
+
+import org.springframework.stereotype.Service;
+
+import com.woowacamp.storage.domain.folder.dto.request.FolderMoveDto;
+import com.woowacamp.storage.domain.folder.entity.FolderMetadata;
+import com.woowacamp.storage.domain.folder.repository.FolderMetadataJpaRepository;
+import com.woowacamp.storage.domain.folder.service.FolderService;
+import com.woowacamp.storage.global.error.ErrorCode;
+import com.woowacamp.storage.global.util.StorageStringUtil;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class FolderFacade {
+
+	private final FolderService folderService;
+	private final FolderMetadataJpaRepository folderMetadataJpaRepository;
+
+	public void moveFolder(Long sourceFolderId, FolderMoveDto dto) {
+		folderService.getFolderJobLock(sourceFolderId, dto);
+		folderService.moveFolder(sourceFolderId, dto);
+	}
+}
