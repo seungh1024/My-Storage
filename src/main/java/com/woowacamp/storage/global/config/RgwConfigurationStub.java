ece4f1a9bc6c198a0fe7358d3bacaa18305e1f33
seungh1024 <seungh1024@naver.com>
2026-01-31 00:05:14 +0900

    refactor: 개발 환경용 프로필 변경

--- src/main/java/com/woowacamp/storage/global/config/RgwConfigurationStub.java
+++ src/main/java/com/woowacamp/storage/global/config/RgwConfigurationStub.java
@@ -11,7 +11,7 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
-@Profile("test")
+@Profile({"test", "local"})
 @Configuration
 public class RgwConfigurationStub {
 	@Bean
